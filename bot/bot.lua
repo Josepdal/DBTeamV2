db15a2e70dfc69a11d6198b9d5a3b7674c7a24d1
Josep <josepdal97@gmail.com>
2016-12-26 21:03:18 +0100

    bot base

---
+++ bot/bot.lua
@@ -0,0 +1,21 @@
+package.path = package.path .. ';.luarocks/share/lua/5.2/?.lua'
+  ..';.luarocks/share/lua/5.2/?/init.lua'..';/root/DBTeam/bot/?.lua'
+package.cpath = package.cpath .. ';.luarocks/lib/lua/5.2/?.so'
+
+require('utils')
+
+local lgi = require ('lgi')
+local notify = lgi.require('Notify')
+notify.init ("Telegram updates")
+
+chats = {}
+
+function do_notify (user, msg)
+    local n = notify.Notification.new(user, msg)
+    n:show ()
+end
+
+function tdcli_update_callback (data)
+    vardump(data)
+    print("a")
+end
