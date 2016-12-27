bbb63564f767781c5a9b8cfac465ec3aa5266655
Harry <jarriztg@users.noreply.github.com>
2016-12-27 15:14:16 -0600

    New launch with supereffects

--- launch.sh
+++ launch.sh
@@ -1,8 +1,4 @@
-#!/usr/bin/env bash
-
-THIS_DIR=$(cd $(dirname $0); pwd)
-cd $THIS_DIR
-
+# Launch created by @Jarriz, @Josepdal and @iicc1)
 config_lines=(
   	'default_profile = "dbteam";'
   	''
@@ -18,91 +14,35 @@ config_lines=(
   	'}'
 )
 
-function install_luarocks() {
-  git clone https://github.com/keplerproject/luarocks.git
-  cd luarocks
-  git checkout tags/v2.3.0-rc2 # Release Candidate
-
-  PREFIX="$THIS_DIR/.luarocks"
-
-  ./configure --prefix=$PREFIX --sysconfdir=$PREFIX/luarocks --force-config
-
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  make build && make install
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting.";exit $RET;
-  fi
-
-  cd ..
-  rm -rf luarocks
-}
-
-function install_rocks() {
-  ./.luarocks/bin/luarocks install luasec
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install lbase64 20120807-3
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install luafilesystem
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install lub
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install luaexpat
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install redis-lua
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install lua-cjson
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install fakeredis
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install xml
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install feedparser
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install serpent
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
+function install_libs_lua() {
+  today=`date +%F`
+  lualibs=(
+    "luasec"
+    "lbase64 20120807-3"
+    "luafilesystem"
+    "lub"
+    "luaexpat"
+    "redis-lua"
+    "lua-cjson"
+    "fakeredis"
+    "xml"
+    "feedparser"
+    "serpent"
+  )
+  if [[ ! -d "logs" ]]; then mkdir logs; fi
+  if [[ -f "logs/logluarocks_${today}.txt" ]]; then rm logs/logluarocks_${today}.txt; fi
+  local i file
+  for i in ${!lualibs[@]}; do
+    printf "\rInstalling lualibs... [${i}/${#lualibs[@]}]\t${lualibs[i]}                         "
+    luarocks install ${lualibs[$i]} &>> logs/logluarocks_${today}.txt
+  done
+  sleep 0.2
+  printf "\rInstalling lualibs... [11/11]"
+  printf "\nLogfile created: `pwd`/logs/logluarocks_${today}.txt\nDone\n"
 }
 
 function install() {
-  git pull
-  git submodule update --init --recursive
-  install_luarocks
-  install_rocks
-
+  install_libs_lua
   if [ -d /mnt/c/Windows ]; then
     echo "Patching bot.lua for Windows 10 support..."
     sed -i '5d' bot/bot.lua
@@ -111,13 +51,6 @@ function install() {
     sed -i '6i\require("bot/permissions")' bot/bot.lua
     sed -i '7i\--File patched to support W10' bot/bot.lua
   fi
-
-}
-
-function is_ready() {
-	if [[ `grep -wi "dbteam" ~/.telegram-cli/$1` ]]; then
-		echo true
-	fi
 }
 
 function do_file() {
@@ -125,18 +58,20 @@ function do_file() {
 	for i in ${!config_lines[@]}; do
 		lines+=${config_lines[$i]}"\n"
 	done
-	echo -e $lines >> ~/.telegram-cli/${1}
+	echo -e $lines > ~/.telegram-cli/${1}
 }
 
 function character() {
-	letter=$(($1 - 1))
-	string=`echo $2`
+	string=`echo "$2"`
 	echo ${string:${letter}:1}
 }
 
+function update() {
+  git checkout .
+  git pull
+}
+
 function subprocess() {
-	if [[ `is_ready "config"` ]]; then
-			rm -r ~/.telegram-cli/config
 			for i in 25 50 75 100; do
 				printf "\rConfiguring... [%i%%]" $i
 				sleep 0.5
@@ -144,13 +79,57 @@ function subprocess() {
 			printf "\nDone\n"
 			do_file config
 			chmod +x ./bin/telegram-cli
-	fi
+}
+
+function start_bot() {
+  ./bin/telegram-cli
+}
+
+function show_logo_slowly() {
+  seconds="0.009"
+  logo1="____  ____ ______"
+  logo2="|    \|  _ )_   _|___ ____   __  __"
+  logo3="| |_  )  _ \ | |/ .__|  _ \_|  \/  |"
+  logo4="|____/|____/ |_|\____/\_____|_/\/\_|  v2"
+  printf "\033[38;5;208m\t"
+  for x in `seq 0 ${#logo1}`; do
+    printf "${logo1:$x:1}"
+    sleep $seconds
+  done
+  printf "\n\t"
+  for x in `seq 0 ${#logo2}`; do
+    printf "${logo2:$x:1}"
+    sleep $seconds
+  done
+  printf "\n\t"
+  for x in `seq 0 ${#logo3}`; do
+    printf "${logo3:$x:1}"
+    sleep $seconds
+  done
+  printf "\n\t"
+  for x in `seq 0 ${#logo4}`; do
+    printf "${logo4:$x:1}"
+    sleep $seconds
+  done
+  printf "\e[36m\n\n"
+}
+
+function show_logo() {
+ #Adding some color. By @iicc1 :D
+ echo -e "\033[38;5;208m"
+ echo -e "      ____  ____ _____                        "
+ echo -e "     |    \|  _ )_   _|___ ____   __  __      "
+ echo -e "     | |_  )  _ \ | |/ .__|  _ \_|  \/  |     "
+ echo -e "     |____/|____/ |_|\____/\_____|_/\/\_|     "
+ echo -e "                                              \033[0;00m"
+ echo -e "\e[36m"
 }
 
 case `character 1 "$1"` in
 -)
 	case `character 2 "$1"` in
 	i)
+    show_logo_slowly
 		install
 		subprocess
 	esac
@@ -160,7 +139,14 @@ esac
 
 case $1 in
   install)
-  install
-  subprocess
+    show_logo_slowly
+    install
+    subprocess
+    exit ;;
+  update)
+    update
+    exit ;;
+  *)
+    show_logo
+    start_bot
 esac
-./bin/telegram-cli
