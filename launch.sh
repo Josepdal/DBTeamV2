2685205f92d1cce74f56062a3c3f3aeb4b148a82
Josep <josepdal97@gmail.com>
2016-12-27 15:30:45 +0100

    luarocks install and update

--- launch.sh
+++ launch.sh
@@ -33,25 +33,156 @@ function character() {
 	echo ${string:${letter}:1}
 }
 
-case `character 1 "$1"` in
--)
-	case `character 2 "$1"` in
-	i)
-	if [[ `is_ready "config"` ]]; then
-		printf "The file config is already configured\nDo you want make the file again? [Y/n] "
-		read opt
-		case $opt in
-		"Y"|"y"|"S"|"s")
-			rm -r ~/.telegram-cli/config
-			for i in 25 50 75 100; do
-				printf "\rConfiguring... [%i%%]" $i
-				sleep 0.5
-			done
-			printf "\nDone\n"
-			do_file config
-			exit
+update() {
+  git pull
+  git submodule update --init --recursive
+  install_rocks
+}
+
+# Will install luarocks on THIS_DIR/.luarocks
+install_luarocks() {
+  git clone https://github.com/keplerproject/luarocks.git
+  cd luarocks
+  git checkout tags/v2.3.0-rc2 # Release Candidate
+
+  PREFIX="$THIS_DIR/.luarocks"
+
+  ./configure --prefix=$PREFIX --sysconfdir=$PREFIX/luarocks --force-config
+
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  make build && make install
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting.";exit $RET;
+  fi
+
+  cd ..
+  rm -rf luarocks
+}
+
+install_rocks() {
+  ./.luarocks/bin/luarocks install luasec
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install lbase64 20120807-3
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install luafilesystem
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install lub
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install luaexpat
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install redis-lua
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install lua-cjson
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install fakeredis
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install xml
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install feedparser
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install serpent
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+}
+
+install() {
+  git pull
+  git submodule update --init --recursive
+  patch -i "patches/disable-python-and-libjansson.patch" -p 0 --batch --forward
+  RET=$?;
+
+  cd tg
+  if [ $RET -ne 0 ]; then
+    autoconf -i
+  fi
+  ./configure && make
+
+  RET=$?; if [ $RET -ne 0 ]; then
+    echo "Error. Exiting."; exit $RET;
+  fi
+  cd ..
+  install_luarocks
+  install_rocks
+  
+  if [ -d /mnt/c/Windows ]; then
+    echo "Patching bot.lua for Windows 10 support..."
+    sed -i '5d' bot/bot.lua
+    sed -i '5d' bot/bot.lua
+    sed -i '5i\require("bot/utils")' bot/bot.lua
+    sed -i '6i\require("bot/permissions")' bot/bot.lua
+    sed -i '7i\--File patched to support W10' bot/bot.lua
+  fi
+  
+}
+
+if [ "$1" = "install" ]; then
+  install
+elif [ "$1" = "update" ]; then
+  update
+else
+	#Adding some color. By @iicc1 :D
+   	echo -e "\033[38;5;208m"
+   	echo -e "      ____  ____ _____                        "
+   	echo -e "     |    \|  _ )_   _|___ ____   __  __      "
+   	echo -e "     | |_  )  _ \ | |/ .__|  _ \_|  \/  |     "
+   	echo -e "     |____/|____/ |_|\____/\_____|_/\/\_|     "
+  	echo -e "                                              \033[0;00m"
+   	echo -e "\e[36m"
+
+	case `character 1 "$1"` in
+	-)
+		case `character 2 "$1"` in
+		i)
+		if [[ `is_ready "config"` ]]; then
+			printf "The file config is already configured\nDo you want make the file again? [Y/n] "
+			read opt
+			case $opt in
+			"Y"|"y"|"S"|"s")
+				rm -r ~/.telegram-cli/config
+				for i in 25 50 75 100; do
+					printf "\rConfiguring... [%i%%]" $i
+					sleep 0.5
+				done
+				printf "\nDone\n"
+				do_file config
+				exit
+			esac
+		fi
 		esac
-	fi
+	;;
 	esac
-;;
-esac
+fi
