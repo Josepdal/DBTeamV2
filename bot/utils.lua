000f8e12b4c1318838bf66ae50edbe0c82fc7b5e
Josep <josepdal97@gmail.com>
2016-12-29 23:50:55 +0100

    added new functions

--- bot/utils.lua
+++ bot/utils.lua
@@ -4,6 +4,9 @@ json = (loadfile "./libs/JSON.lua")()
 
 --redis = (loadfile "./libs/redis.lua")()
 
+function dl_cb (arg, data)
+end
+
 function vardump(value, depth, key)
     local linePrefix = ""
     local spaces = ""
@@ -51,50 +54,8 @@ function oldtg(data)
     msg.date = data.message_.date_
     msg.id = data.message_.id_
     msg.unread = false
+    msg.reply_id = data.message_.reply_to_message_id_
     return msg
-end
-
-function send_msg(chat_id, text, cb_function, cb_extra)
-    tdcli_function ({ID="SendMessage", chat_id_=chat_id, reply_to_message_id_=0, disable_notification_=0, from_background_=1, reply_markup_=nil, input_message_content_={ID="InputMessageText", text_=text, disable_web_page_preview_=1, clear_draft_=0, entities_={[0]={ID="MessageEntityBold", offset_=0, length_=9}}}}, dl_cb, nil)
-end
-
-function reply_msg(chat_id, text, msg_id, cb_extra)
-    tdcli_function ({ID="SendMessage", chat_id_=chat_id, reply_to_message_id_=msg_id, disable_notification_=0, from_background_=1, reply_markup_=nil, input_message_content_={ID="InputMessageText", text_=text, disable_web_page_preview_=1, clear_draft_=0, entities_={[0]={ID="MessageEntityBold", offset_=0, length_=9}}}}, dl_cb, nil)
-end
-
--- Same as send_large_msg_callback but friendly params
-function send_large_msg(destination, text)
-    local cb_extra = {
-        destination = destination,
-        text = text
-    }
-    send_large_msg_callback(cb_extra, true)
-end
-
--- If text is longer than 4096 chars, send multiple msg.
--- https://core.telegram.org/method/messages.sendMessage
-function send_large_msg_callback(cb_extra, success, result)
-    local text_max = 4096
-
-    local destination = cb_extra.destination
-    local text = cb_extra.text
-    local text_len = string.len(text)
-    local num_msg = math.ceil(text_len / text_max)
-
-    if num_msg <= 1 then
-        send_msg(destination, text, ok_cb, false)
-    else
-
-        local my_text = string.sub(text, 1, 4096)
-        local rest = string.sub(text, 4096, text_len)
-
-        local cb_extra = {
-            destination = destination,
-            text = rest
-        }
-
-        send_msg(destination, my_text, send_large_msg_callback, cb_extra)
-    end
 end
 
 function serialize_to_file(data, file, uglify)
@@ -131,4 +92,21 @@ end
 function get_receiver(msg)
     return msg.from.id
 
+end
+
+function getChatId(chat_id)
+    print("chat_id")
+  local chat = {}
+  local chat_id = tostring(chat_id)
+
+  if chat_id:match('^-100') then
+    local channel_id = chat_id:gsub('-100', '')
+    chat = {ID = channel_id, type = 'channel'}
+  else
+    local group_id = chat_id:gsub('-', '')
+    chat = {ID = group_id, type = 'group'}
+  end
+  vardump(chat)
+
+  return chat
 end
