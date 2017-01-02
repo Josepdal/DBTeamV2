e4998d8c08c01f3718b5113604f6b6427802aa1d
Josep <josepdal97@gmail.com>
2017-01-02 22:04:20 +0100

    get id and reply ids

--- plugins/id.lua
+++ plugins/id.lua
@@ -6,8 +6,20 @@
 --                                                --
 ----------------------------------------------------
 
+function send_ID_by_reply(channel_id, message_id)
+    get_msg_info(channel_id, message_id, getID_by_reply_cb, false)
+end
+
+function getID_by_reply_cb(arg, msg)
+    send_msg(msg.chat_id_, "*User ID:* " .. msg.sender_user_id_ .. "\n*Chat ID:* " .. msg.chat_id_, "md")
+end
+
 local function run(msg, matches)
-    send_msg(msg.to.id, "*User ID:* " .. msg.from.id .. "\n*Chat ID:* " .. msg.to.id, "md")
+    if msg.reply_id == 0 then
+    	send_msg(msg.to.id, "*User ID:* " .. msg.from.id .. "\n*Chat ID:* " .. msg.to.id, "md")
+    else
+    	send_ID_by_reply(msg.to.id, msg.reply_id)
+    end
 end
 
 return {
