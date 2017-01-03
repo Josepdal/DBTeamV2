f0faef42bd1d0dce9bd01e2b6a3d50ed527698c7
Harry <rickygamberroz@hotmail.com>
2017-01-03 16:17:30 -0600

    Links.lua added

---
+++ data/links.lua
@@ -0,0 +1,18 @@
+blacklist = {
+  ["default"] = {
+    "telegram.me",
+    "telegra.ph",
+    "@channel"
+  }
+  ["search"] = {
+    "google.com"
+  }
+}
+
+whitelist = {
+  ["default"] = {
+    "@DBTeam",
+    "@DBTeamES",
+    "@DBTeamEN"
+  }
+}
