7296105ae58c3aa06d9b6f32105a3bb87f70ad15
saman9074 <saman9074@yahoo.com>
2017-04-03 09:24:27 +0430

    Update english_lang.lua

--- lang/english_lang.lua
+++ lang/english_lang.lua
@@ -35,6 +35,7 @@ local function run(msg, matches)
 
 		-- stats.lua
 		set_text(LANG, 'stats', '*Chat stats*')
+		set_text(LANG, 'statsCommand', 'stats')
 
 		-- settings.lua --
 		set_text(LANG, 'user', 'User')
