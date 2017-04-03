a785e11b966dce64c7027325d816d837e14c3ae1
saman9074 <saman9074@yahoo.com>
2017-04-03 15:23:14 +0430

    Update and rename warn.lua to fun.lua

---
+++ plugins/fun.lua
@@ -0,0 +1,47 @@
+require("json");
+require("socket.http")
+require("socket.url")
+
+
+
+function babelfish(lang, query)
+    lang  = lang  or "en"
+    query = query or ""
+
+    if lang == "en" or query == "" then
+        return query
+    end
+
+    local result, status = socket.http.request(
+        "http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q="
+        .. socket.url.escape(query)
+        .. "&langpair=en|"
+        .. lang
+    )
+    if not (status == 200 or status == 201) then
+        return "api error: " .. status
+    end
+
+    local data = json.decode(result)
+    if data.responseStatus == 200 then
+        return data.responseData.translatedText
+    else
+        return "status " .. data.responseStatus .. ": " .. data.responseDetails
+    end
+end
+
+
+
+local function run(msg, matches)
+        if matches[1] == "tr" and if matches[2] then
+			local text = babelfish("en", matches[2])
+                 send_msg(msg.to.id, text, "md")
+        end
+end
+return {
+        patterns = {
+                "^[!/#](tr)$"
+        },
+        run = run,
+        pre_process = pre_process
+}
