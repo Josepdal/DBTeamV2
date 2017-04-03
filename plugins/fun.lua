16a077da2daf81b0c82bd92b86bd6d96f4f4f795
saman9074 <saman9074@yahoo.com>
2017-04-03 15:49:19 +0430

    Update fun.lua

--- plugins/fun.lua
+++ plugins/fun.lua
@@ -1,47 +1,44 @@
-require("json");
-require("socket.http")
-require("socket.url")
+do
 
-
-
-function babelfish(lang, query)
-    lang  = lang  or "en"
-    query = query or ""
-
-    if lang == "en" or query == "" then
-        return query
-    end
-
-    local result, status = socket.http.request(
-        "http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q="
-        .. socket.url.escape(query)
-        .. "&langpair=en|"
-        .. lang
-    )
-    if not (status == 200 or status == 201) then
-        return "api error: " .. status
-    end
-
-    local data = json.decode(result)
-    if data.responseStatus == 200 then
-        return data.responseData.translatedText
-    else
-        return "status " .. data.responseStatus .. ": " .. data.responseDetails
-    end
+local function get_9GAG()
+  local url = "http://api-9gag.herokuapp.com/"
+  local b,c = http.request(url)
+  if c ~= 200 then return nil end
+  local gag = json:decode(b)
+  -- random max json table size
+  local i = math.random(#gag)
+  local link_image = gag[i].src
+  local title = gag[i].title
+  if link_image:sub(0,2) == '//' then
+    link_image = msg.text:sub(3,-1)
+  end
+  return link_image, title
 end
 
-
-
+local function send_title(cb_extra, success, result)
+  if success then
+    send_msg(cb_extra[1], cb_extra[2], ok_cb, false)
+  end
+end
+--[[
 local function run(msg, matches)
-        if matches[1] == "tr" and matches[2] and  matches[3] then
-			local text = babelfish(matches[2], matches[3])
-                 send_msg(msg.to.id, text, "md")
+ 
+  return false
+end
+]]--
+local function run(msg, matches)
+        if matches[1] == "9gag" then
+			 local receiver = get_receiver(msg)
+  local url, title = get_9GAG()
+  send_photo_from_url(receiver, url, send_title, {receiver, title})
         end
 end
 return {
         patterns = {
-               "^[!/#](tr) (.*)$"
+              "^!9gag$"
         },
         run = run,
         pre_process = pre_process
 }
+
+end
