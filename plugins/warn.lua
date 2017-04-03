10579670f0479f34a0c781032609ad4cfaf0c636
saman9074 <saman9074@yahoo.com>
2017-04-03 14:24:46 +0430

    Update warn.lua

--- plugins/warn.lua
+++ plugins/warn.lua
@@ -1,1 +1,12 @@
-
+local function run(msg, matches)
+        if matches[1] == "setwarn" then
+                send_msg(msg.to.id, "warnSet", "md")
+        end
+end
+return {
+        patterns = {
+                "^[!/#](setwarn)$"
+        },
+        run = run,
+        pre_process = pre_process
+}
