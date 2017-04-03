ad2998024b101876b1ca10b0d77d65a30d701bf5
saman9074 <saman9074@yahoo.com>
2017-04-03 18:20:04 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -66,8 +66,9 @@ local function pre_process(msg)
         return msg
 end
 
-statsCommandT=lang_text(msg.to.id, 'statsCommand')
+
 local function run(msg, matches)
+	statsCommandT=lang_text(msg.to.id, 'statsCommand')
         if matches[1] == lang_text(msg.to.id, 'statsCommand') then
                 ranking = get_ranking(msg.to.id, lang_text(msg.to.id, 'stats'))
                 send_msg(msg.to.id, ranking, "md")
